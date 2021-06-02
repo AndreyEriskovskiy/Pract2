435c1a054271a710e1662aae14f5dbbf7cc4dca5
Andrey Eriskovskiy 20PI1 <80524924+AndreyEriskovskiy@users.noreply.github.com>
2021-06-03 00:50:41 +0300

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,38 @@
+include <iostream>
+#include <cmath>
+using namespace std;
+bool prostoi(long long unsigned  j)
+{
+    long long unsigned i;
+    if (j==0) 
+        return false;
+    for (i=2; i<=sqrt(j); i++) {
+        if (j % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int BitOne(long long unsigned j)
+{
+    long long unsigned i;
+    int bit = 0;
+    for (i=0; i<64; i++) {
+        if((j>>i)%2==1) 
+            bit++;
+    }
+    return bit;
+}
+int main()
+{
+    long long unsigned int i;
+    long long unsigned int limit = 0xFFFFFFFFFFFFFFFF;
+    long long unsigned int min_value=0x80000001;
+    for(i=min_value; i<limit; i++) {
+        if ((BitOne(i) == 3) && (prostoi(i))) {
+            cout<<"Decimal: "<<dec<<i<<"\tHexadecimal: "<<hex<<i<<endl;
+        
+        }
+    }
+    return 0;
+}
